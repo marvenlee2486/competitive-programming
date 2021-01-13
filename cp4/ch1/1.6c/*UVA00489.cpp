3e08306bf924bcdc4375d38ab014ed4a9bd45ccd
marvenlee2486 <65748142+marvenlee2486@users.noreply.github.com>
2021-01-13 14:58:00 +0800

    Create *UVA00489.cpp

---
+++ cp4/ch1/1.6c/*UVA00489.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> ii;
+typedef pair<ll, ii> iii;
+typedef string str;
+typedef vector<ll>  vl;
+typedef vector< vector<ll> > vvl;
+typedef vector<ii> vii;
+typedef vector< vector<ii> > vvii;
+typedef vector<iii> viii;
+typedef vector< vector<iii> > vviii;
+typedef vector<str>  vs;
+typedef vector< vector<str> > vvs;
+typedef long double ld;
+
+//typelower_bounddef priority_queue<long long> pq;
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+typedef set<int>::iterator sit;
+typedef map<int,int>::iterator mit;
+typedef vector<int>::iterator vit;
+
+#define elif else if
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define FOR(i,v) for(auto i:v)
+#define rep(i,a,b) for(int i=(a);i<(b);i++)
+#define rrep(i,a,b) for(int i=(a);i>=(b);i--)
+#define repn(i,a,b) for(int i=(a);i<=(b);i++)
+#define repin(i,a,b,x) for(int i=(a);i<(b);i+=x)
+#define repnin(i,a,b,x) for(int i=(a);i<=(b);i+=x)
+#define MAX(a,b) a=max(a,b)
+#define MIN(a,b) a=min(a,b)
+#define fbo find_by_order
+#define ook order_of_key
+#define ALL(v) v.begin(),v.end()
+#define FILL(arr,x) memset(arr,x,sizeof arr);
+// if else int i = (number <0) ? 1:0 -  if (number <0) i=1 else 0
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+int main()
+{ 
+ //ios_base::sync_with_stdio(0);
+ //cin.tie(0);
+ //cout.tie(0);
+ //freopen("OUT.txt","w",stdout);
+  int n;
+  while(cin >> n && n !=-1){
+    str ans,guess;
+    cin >> ans >> guess;
+    set<char> st;
+    int cnt=0,wrong=0;
+    rep(i,0,ans.size()) st.insert(ans[i]);
+    int sta[26];
+    memset(sta,0,sizeof sta);
+    cout <<"Round " << n << endl;
+    rep(i,0,guess.size()){
+      if(st.find(guess[i])==st.end() && sta[guess[i]-'a']!=1) {
+        wrong++;
+        sta[guess[i]-'a']=1;
+      }
+      elif (sta[guess[i]-'a']!=1) {cnt+=1;sta[guess[i]-'a']=1;
+      }
+
+
+      if(wrong==7){
+        cout <<"You lose.\n";
+        break;
+      }
+      else if(cnt==st.size()){
+        cout << "You win.\n";
+        break;
+      }
+    }
+    
+    if(cnt!=st.size() && wrong !=7) cout << "You chickened out.\n";
+  }
+}
