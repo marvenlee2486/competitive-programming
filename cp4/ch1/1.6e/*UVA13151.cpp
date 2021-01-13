9afd0a9d623a98aee0a11c5aba69cb5b3fbd4197
marvenlee2486 <65748142+marvenlee2486@users.noreply.github.com>
2021-01-13 19:19:33 +0800

    Create *UVA13151.cpp

---
+++ cp4/ch1/1.6e/*UVA13151.cpp
@@ -0,0 +1,138 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> ii;
+typedef pair<ll, ii> iii;
+typedef string str;
+typedef vector<ll>  vl;
+typedef vector< vector<ll> > vvl;
+typedef vector<ii> vii;
+typedef vector< vector<ii> > vvii;
+typedef vector<iii> viii;
+typedef vector< vector<iii> > vviii;
+typedef vector<str>  vs;
+typedef vector< vector<str> > vvs;
+typedef long double ld;
+
+//typelower_bounddef priority_queue<long long> pq;
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+typedef set<int>::iterator sit;
+typedef map<int,int>::iterator mit;
+typedef vector<int>::iterator vit;
+
+#define elif else if
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define FOR(i,v) for(auto i:v)
+#define rep(i,a,b) for(int i=(a);i<(b);i++)
+#define rrep(i,a,b) for(int i=(a);i>=(b);i--)
+#define repn(i,a,b) for(int i=(a);i<=(b);i++)
+#define repin(i,a,b,x) for(int i=(a);i<(b);i+=x)
+#define repnin(i,a,b,x) for(int i=(a);i<=(b);i+=x)
+#define MAX(a,b) a=max(a,b)
+#define MIN(a,b) a=min(a,b)
+#define fbo find_by_order
+#define ook order_of_key
+#define ALL(v) v.begin(),v.end()
+#define FILL(arr,x) memset(arr,x,sizeof arr);
+// if else int i = (number <0) ? 1:0 -  if (number <0) i=1 else 0
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+int main()
+{ 
+ //ios_base::sync_with_stdio(0);
+ //cin.tie(0);
+ //cout.tie(0);
+// freopen("OUT.txt","w",stdout);
+  str s;int t;
+  const str value ="0123456789ABCDEF";
+  int k=0;
+  while(cin >> s >> t && t){
+
+    
+    int i=0,cnt=0;
+    if(s[0]=='0'){
+      if(s[1]=='x'){
+          rep(j,2,s.size()){
+            i*=16;
+            i+= value.find(s[j]);
+          }
+      }
+      else{
+          rep(j,1,s.size()){
+            i*=8;
+            i+= value.find(s[j]);
+          }
+      }
+    }
+    else{
+      rep(j,0,s.size()){
+            i*=10;
+            i+= value.find(s[j]);
+          }
+
+    }
+    while(t--){
+      
+        str op;
+    cin >> op >> s;
+   
+    int ans=0;
+    if(s[0]=='0'){
+      if(s[1]=='x'){
+          rep(j,2,s.size()){
+            ans*=16;
+            ans+= value.find(s[j]);
+          }
+      }
+      else{
+          rep(j,1,s.size()){
+            ans*=8;
+            ans+= value.find(s[j]);
+          }
+      }
+    }
+
+    else{
+      rep(j,0,s.size()){
+            ans*=10;
+            ans+= value.find(s[j]);
+          }
+
+    }
+
+
+    if(op=="++i"){
+      if(ans==i+1) cnt++;
+  
+       i=ans;
+    }
+    elif(op=="i++"){
+      if(ans==i) cnt++;
+      i=ans+1;
+   
+    }
+    elif(op=="i--"){
+      if(ans==i)cnt++;
+      i=ans-1;
+    }
+    elif(op=="i"){
+      if(ans==i) cnt++;
+      i=ans;
+    
+    }
+    elif(op=="--i") {
+      if(ans==i-1) cnt++;
+      i=ans;
+      
+    }
+    
+    }
+    cout << cnt << endl;
+  }
+
+}
