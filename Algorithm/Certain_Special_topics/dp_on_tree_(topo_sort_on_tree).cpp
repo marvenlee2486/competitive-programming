fdb5e5ae257e49e12c81c8560a55cdb2d5571b0e
marvenlee2486 <leezongyu6878@gmail.com>
2021-12-13 03:13:52 +0800

    commited on 13 Dec

--- Algorithm/Certain_Special_topics/dp_on_tree_(topo_sort_on_tree).cpp
+++ Algorithm/Certain_Special_topics/dp_on_tree_(topo_sort_on_tree).cpp
@@ -43,7 +43,7 @@ const ld EPS = 1e-9;
 const ll N = 1e6+2;
 
 #ifndef ONLINE_JUDGE
-#define LOG(message) cerr << message << "\n";
+#define LOG(message) cerr << message << endl;
 #else 
 #define LOG(message)
 #endif
@@ -56,53 +56,11 @@ template <typename... Ts> ll addm(ll x, Ts... ys) { return mod(x + addm(ys...));
 ll mulm(ll x) { return x; }
 template <typename... Ts> ll mulm(ll x, Ts... ys) { return mod(x * mulm(ys...)); }
 
-bool clash(int xk, int yk, int x1, int y1, int x2 , int y2){
-    if( (x1==xk  && y1==yk)||
-     (x2==xk && y2 == yk) || 
-     (x1==x2 && y1 == y2)||
-     x1<1 || y1<1 || x1 >8 || y1 >8 || 
-     x2<1 || y2<1 || x2>8 || y2>8
-    ) return false;
-
-    int dx[8] = {0,0,1,1,1,-1,-1,-1};
-    int dy[8] = {1,-1,0,-1,1,0,-1,1};
-
-    for(int d=0;d<8;d++){
-        int nx = dx[d]+xk;
-        int ny = dy[d]+yk;
-
-        if(nx<1 || ny<1 || nx>8|| ny>8) continue;
-        
-        if( (!(x1==nx && y1==ny) && (nx == x1||ny == y1 ))||
-          (!(x2==nx && y2==ny) && (nx == x2||ny == y2 ))
-          ) continue;
-
-          return false;
-    }
-    //LOG( xk << " " << yk << " "<< x1 << " " << y1  << " " << x2 << " " << y2 );
-    return true;
-}
-
 int main()
 {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-  int t;
-  cin >> t;
- while(t--){
- int xk,yk,x1,y1,x2,y2;
- cin >> xk >> yk >> x1 >> y1 >> x2 >> y2;
-
-    if(  ( (y1!=y2 || !(x2 > min(x1,xk) && x2 < max(x1,xk) ) ) && clash(xk,yk,xk,y1,x2,y2) )|| ((x1!=x2 || !(y2 > min(y1,yk) && y2 < max(y1,yk) )) && clash(xk,yk,x1,yk,x2,y2) ) ||
-     ((y1!=y2 || !(x1 > min(x2,xk) && x1 < max(x2,xk) ))&& clash(xk,yk,xk,y2,x1,y1)) ||  ((x1!=x2 || !(y1 > min(y2,xk) && y1 < max(y2,xk))) && clash(xk,yk,x2,yk,x1,y1) )
-     ){
-         cout <<"YES\n";
-     }
-     else cout << "NO\n";
-    
- }
-
   
   return 0;
 }
---
+++ Contests/README.md
@@ -0,0 +1,17 @@
+# List of Contests Availables in this directory
+
+## CodeForces
+
+
+## AtCoder
+
+
+## Hackerearth
+
+
+
+
+
+# List of Problems of marvenlee's Likes
+CF_Div1+Div2_#758 Problem C: https://codeforces.com/contest/1608/problem/C
+CF_Div1+Div2_#758 Problem D: https://codeforces.com/contest/1608/problem/D
